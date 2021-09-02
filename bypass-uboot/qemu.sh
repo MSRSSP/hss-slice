2d45610eb5b9559e77f2cf3033893ba77d10c74e
ziqiao <ziqiaozhou@microsoft.com>
2021-09-02 17:35:00 +0000

    Add single.dts device tree for polarfire board.
    bypass-uboot/Makefile provide targets to build hss payload, to format image in
    MMC/SD card.

---
+++ bypass-uboot/qemu.sh
@@ -0,0 +1,11 @@
+QEMU=${TOP}/qemu/build/qemu-system-riscv64
+HSS=${TOP}/slice-hss/Default-qemu/hss-envm-wrapper.bin
+SD=$1 #build/slice-qemu-sd.img
+DEBUG_OPT="-d opensbi -D log.txt -s"
+OPT="-monitor telnet:127.0.0.1:4322,server,nowait"
+${QEMU} ${DEBUG_OPT} -M microchip-icicle-kit -smp 5 -m 4G \
+	-bios ${HSS}	-sd ${SD} ${OPT} \
+	-display none -serial stdio \
+	-serial telnet:localhost:5431,server,nowait \
+	-serial telnet:localhost:5432,server,nowait \
+	-serial telnet:localhost:5433,server,nowait
